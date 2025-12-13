f4554b48e8f1ec51bc4eb51fa2a68a3212691527
Vasanth386 <24331a05d6@mvgrce.edu.in>
2025-12-13 22:37:30 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,13 @@
+
+a = 10
+print("integer value:", a)
+print("type of a:",type(a))
+b = 10.5
+print("F=float value:", b)
+print("Type of b:",type(b))
+c = 'A'
+print("character value:", c)
+print("type of c:", type(c))
+d = "hello"
+print("string value:",d)
+print("type of d:",type(d))
