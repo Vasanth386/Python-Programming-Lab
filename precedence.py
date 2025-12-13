0ff833a97afff07a25eee173fe09452e4e464872
Vasanth386 <24331a05d6@mvgrce.edu.in>
2025-12-13 22:49:10 +0530

    Add files via upload

---
+++ precedence.py
@@ -0,0 +1,9 @@
+
+result = 5 + 3 * 2
+print("Result of 5 + 3 * 2 is:",result)
+result = 2 * 3 ** 2
+print("Result of 2 * 3 ** 2 is:",result)
+result = 2 ** 3 ** 2
+print("Result of 2 ** 3 ** 2 is:",result)
+result = (2 ** 3) ** 2
+print("Result of (2 ** 3) ** 2 is:",result)
